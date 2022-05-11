eee9ede10a1467a7a47577d590d8dac51ad3ae6a
Ross Hazelett <rhhazelett@gmail.com>
2022-05-10 23:35:03 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify Clone</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className="">
+        <h1>Hello World</h1>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      {/* Player */}
+    </div>
+  )
+}
+
+export default Home
